ca1f8eb95870989b56627a08cb920a8a03fd1e19
takumin0423 <takumin0423baske@gmail.com>
2024-04-14 23:51:02 +0900

    Pick

--- index.ts
+++ index.ts
@@ -1,8 +1,30 @@
 /* _____________ Your Code Here _____________ */
 
-type HelloWorld = string; // expected to be a string
+type MyPick<T, K extends keyof T> = {
+  [key in K]: T[key];
+};
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect, NotAny } from "@type-challenges/utils";
+import type { Equal, Expect } from "@type-challenges/utils";
 
-type cases = [Expect<NotAny<HelloWorld>>, Expect<Equal<HelloWorld, string>>];
+type cases = [
+  Expect<Equal<Expected1, MyPick<Todo, "title">>>,
+  Expect<Equal<Expected2, MyPick<Todo, "title" | "completed">>>,
+  // @ts-expect-error
+  MyPick<Todo, "title" | "completed" | "invalid">
+];
+
+interface Todo {
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
+interface Expected1 {
+  title: string;
+}
+
+interface Expected2 {
+  title: string;
+  completed: boolean;
+}
