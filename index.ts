bd1a52e16daa4d2acf1e3b4a9ebd804d43ef29f3
takumin0423 <takumin0423baske@gmail.com>
2024-04-14 23:32:31 +0900

    Hello World

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log("test");
+/* _____________ Your Code Here _____________ */
+
+type HelloWorld = string; // expected to be a string
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect, NotAny } from "@type-challenges/utils";
+
+type cases = [Expect<NotAny<HelloWorld>>, Expect<Equal<HelloWorld, string>>];
