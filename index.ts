1453fce52fb0d9194967821acd4ab72b87edab4b
takumin0423 <takumin0423baske@gmail.com>
2024-05-09 19:53:02 +0900

    First of Array

--- index.ts
+++ index.ts
@@ -1,45 +1,20 @@
 /* _____________ Your Code Here _____________ */
 
-type TupleToObject<T extends readonly PropertyKey[]> = {
-  [P in T[number]]: P;
-};
+type First<T extends any[]> = T extends [] ? never : T[0];
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from "@type-challenges/utils";
 
-const tuple = ["tesla", "model 3", "model X", "model Y"] as const;
-const tupleNumber = [1, 2, 3, 4] as const;
-const sym1 = Symbol(1);
-const sym2 = Symbol(2);
-const tupleSymbol = [sym1, sym2] as const;
-const tupleMix = [1, "2", 3, "4", sym1] as const;
-
 type cases = [
-  Expect<
-    Equal<
-      TupleToObject<typeof tuple>,
-      {
-        tesla: "tesla";
-        "model 3": "model 3";
-        "model X": "model X";
-        "model Y": "model Y";
-      }
-    >
-  >,
-  Expect<Equal<TupleToObject<typeof tupleNumber>, { 1: 1; 2: 2; 3: 3; 4: 4 }>>,
-  Expect<
-    Equal<
-      TupleToObject<typeof tupleSymbol>,
-      { [sym1]: typeof sym1; [sym2]: typeof sym2 }
-    >
-  >,
-  Expect<
-    Equal<
-      TupleToObject<typeof tupleMix>,
-      { 1: 1; "2": "2"; 3: 3; "4": "4"; [sym1]: typeof sym1 }
-    >
-  >
+  Expect<Equal<First<[3, 2, 1]>, 3>>,
+  Expect<Equal<First<[() => 123, { a: string }]>, () => 123>>,
+  Expect<Equal<First<[]>, never>>,
+  Expect<Equal<First<[undefined]>, undefined>>
 ];
 
-// @ts-expect-error
-type error = TupleToObject<[[1, 2], {}]>;
+type errors = [
+  // @ts-expect-error
+  First<"notArray">,
+  // @ts-expect-error
+  First<{ 0: "arrayLike" }>
+];
