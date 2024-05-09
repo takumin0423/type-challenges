7034c72e41e524486d38b895d71d21ce87d43dfd
takumin0423 <takumin0423baske@gmail.com>
2024-05-09 19:43:46 +0900

    Tuple to Object

--- index.ts
+++ index.ts
@@ -1,19 +1,45 @@
 /* _____________ Your Code Here _____________ */
 
-type MyReadonly<T> = {
-  readonly [P in keyof T]: T[P];
+type TupleToObject<T extends readonly PropertyKey[]> = {
+  [P in T[number]]: P;
 };
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from "@type-challenges/utils";
 
-type cases = [Expect<Equal<MyReadonly<Todo1>, Readonly<Todo1>>>];
+const tuple = ["tesla", "model 3", "model X", "model Y"] as const;
+const tupleNumber = [1, 2, 3, 4] as const;
+const sym1 = Symbol(1);
+const sym2 = Symbol(2);
+const tupleSymbol = [sym1, sym2] as const;
+const tupleMix = [1, "2", 3, "4", sym1] as const;
 
-interface Todo1 {
-  title: string;
-  description: string;
-  completed: boolean;
-  meta: {
-    author: string;
-  };
-}
+type cases = [
+  Expect<
+    Equal<
+      TupleToObject<typeof tuple>,
+      {
+        tesla: "tesla";
+        "model 3": "model 3";
+        "model X": "model X";
+        "model Y": "model Y";
+      }
+    >
+  >,
+  Expect<Equal<TupleToObject<typeof tupleNumber>, { 1: 1; 2: 2; 3: 3; 4: 4 }>>,
+  Expect<
+    Equal<
+      TupleToObject<typeof tupleSymbol>,
+      { [sym1]: typeof sym1; [sym2]: typeof sym2 }
+    >
+  >,
+  Expect<
+    Equal<
+      TupleToObject<typeof tupleMix>,
+      { 1: 1; "2": "2"; 3: 3; "4": "4"; [sym1]: typeof sym1 }
+    >
+  >
+];
+
+// @ts-expect-error
+type error = TupleToObject<[[1, 2], {}]>;
